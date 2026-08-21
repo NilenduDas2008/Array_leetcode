24d1d40a06b102b33f24fe7dd7fa9346b0b4dfdc
Nilendu Das <nilendu438@gmail.com>
2026-08-21 12:10:34 +0530

    commit

---
+++ maximum_subarray.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxSubArray(vector<int>& arr) {
+    int currentSum = arr[0];
+    int maxSum = arr[0];
+
+    for (int i = 1; i < arr.size(); i++) {
+        currentSum = max(arr[i], currentSum + arr[i]);
+        maxSum = max(maxSum, currentSum);
+    }
+
+    return maxSum;
+}
+
+int main() {
+    vector<int> arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+    cout << maxSubArray(arr) << endl;
+
+    return 0;
+}
+
+//src chatgpt

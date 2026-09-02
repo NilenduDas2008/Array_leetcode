fa739c255d483461d2c9505389ed6b38ad1b4911
Nilendu Das <nilendu438@gmail.com>
2026-09-02 11:33:14 +0530

    commit

---
+++ Intersection_of_Two_Arrays_II.py
@@ -0,0 +1,33 @@
+from collections import Counter
+
+m = int(input())
+nums1 = []
+for i in range(m):
+    value1  = int(input())
+    nums1.append(value1)
+
+n = int(input())
+nums2 = []
+for i in range(n):  
+    value2 = int(input())
+    nums2.append(value2)
+
+freq1 = Counter(nums1)
+freq2 = Counter(nums2)
+
+intersection_number = freq1 & freq2
+result = list(intersection_number.elements())
+print(result)
+
+#Time: O(m + n + k)  ,  Space: O(m + n) in the worst case.
+
+
+
+
+
+
+
+
+
+
+

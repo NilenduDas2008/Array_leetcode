5f42d756d837d7f46ed511480b8c27dba89d166b
Nilendu Das <nilendu438@gmail.com>
2026-09-03 10:09:01 +0530

    commit

--- Third_Maximum_Number.py
+++ Third_Maximum_Number.py
@@ -1,4 +1,3 @@
-#i am solving leetcode 414 third maximum number now give me hint as a interviewer
 # n = int(input())
 # arr = []
 # for i in range(n):
@@ -13,9 +12,26 @@
 # Time Complexity: O(n log n) ; Space complexity: O(n). the problem can be solved with T.C: O(n) i mean without sorting 
 
 n = int(input())
-arr = []
-for i in range(n):
-    value = int(input())
-    arr.append(value)
 
-nums = list(set(arr))
+max1 = max2 = max3 = float('-inf')
+
+for _ in range(n):
+    x = int(input())
+    
+    if x == max1 or x == max2 or x == max3:
+        continue  # duplicate hole, podium touch korbo na
+    
+    if x > max1:
+        max3 = max2
+        max2 = max1
+        max1 = x
+    elif x > max2:
+        max3 = max2
+        max2 = x
+    elif x > max3:
+        max3 = x
+
+if max3 == float('-inf'):
+    print(max1)
+else:
+    print(max3)

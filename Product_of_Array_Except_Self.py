4a5249acc1e6beb1d0fcd825a831d191537034a1
Nilendu Das <nilendu438@gmail.com>
2026-09-12 13:26:53 +0530

    commit

---
+++ Product_of_Array_Except_Self.py
@@ -0,0 +1,29 @@
+import math
+n = int(input())
+nums = []
+
+for i in range(n):
+    value = int(input())
+    nums.append(value)
+# result = []
+# for i in range(n):
+#     skip_index = i
+#     result.append(math.prod(nums[:skip_index] + nums[skip_index + 1:]))
+# print(result)
+# this code has T.C: O(N^2) so time limit exceeds .
+
+left_prod = 1
+right_prod = 1
+result = [1]*len(nums)
+
+for i in range(len(nums)):
+    result[i]   = left_prod
+    left_prod *= nums[i]
+
+for i in range(len(nums)-1, -1, -1):
+    result[i] *= right_prod
+    right_prod *= nums[i]
+print(result)
+
+# O(N) time
+# O(N) space
